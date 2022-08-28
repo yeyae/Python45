cfc193dd694771ca9465354019233ecbe03f6b40
yeyae <whyjinny@naver.com>
2022-08-28 22:21:27 +0900

    python

---
+++ 45.py
@@ -0,0 +1,13 @@
+#print 자세히 알기
+a = 123
+print(a)
+
+a = "python"
+print(a)
+
+a = [1,2,3]
+print(a)
+
+#큰 따옴표("")로 둘러싸인 문자열은 + 연산과 동일
+print("Life" "is" "too short")
+print("Life"+"is"+"too short")
