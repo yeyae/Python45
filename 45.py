7a14f3b6bfe0d33fc4eb0e7c1a8c0cc8b6f69299
JINNY <103866627+yeyae@users.noreply.github.com>
2022-08-30 19:57:32 +0900

    Update 45.py

--- 45.py
+++ 45.py
@@ -1,16 +1,21 @@
 #print 자세히 알기
 a = 123
-print(a)
+  print(a)
 
 a = "python"
-print(a)
+  print(a)
 
 a = [1,2,3]
-print(a)
+  print(a)
 
 #큰 따옴표("")로 둘러싸인 문자열은 + 연산과 동일
-print("Life" "is" "too short")
-print("Life"+"is"+"too short")
+    print("Life" "is" "too short")
+    print("Life"+"is"+"too short")
 
 #문자열 띄어쓰기는 콤마로 한다.
-print("life", is", "too", "short")
+    print("life", is", "too", "short")
+      
+#한 줄에 결과값 출력하기
+for i in range(10):
+    print(i, end= ' ')
+    
